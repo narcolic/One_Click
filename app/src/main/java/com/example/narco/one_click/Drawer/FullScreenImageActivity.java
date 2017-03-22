c6108af0514e53375748f358c694c81d2e277507
narcolic <narcolic832@gmail.com>
2017-03-22 13:36:41 +0000

    postcards+shareimage

---
+++ app/src/main/java/com/example/narco/one_click/Drawer/FullScreenImageActivity.java
@@ -0,0 +1,205 @@
+package com.example.narco.one_click.Drawer;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.etiennelawlor.imagegallery.library.enums.PaletteColorType;
+import com.example.narco.one_click.R;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class FullScreenImageActivity extends AppCompatActivity {
+    File file;
+    private PaletteColorType paletteColorType;
+    @BindView(R.id.share)
+    Button _shareButton;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fullscreen);
+        ButterKnife.bind(this);
+        paletteColorType = PaletteColorType.VIBRANT;
+        final LinearLayout bgLinearLayout = (LinearLayout) findViewById(R.id.bgLinearLayout);
+
+
+        file = (File) this.getIntent().getExtras().getSerializable("FILE");
+
+        final ImageView imageView = new ImageView(this);
+        LinearLayout layout = new LinearLayout(this);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.setMargins(200, 250, 200, 250);
+        Picasso.with(this)
+                .load(file)
+                .resize(1800, 1100)
+                .centerCrop()
+                .into(imageView, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        Bitmap bitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+                        Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+                            public void onGenerated(Palette palette) {
+                                applyPalette(palette, bgLinearLayout);
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onError() {
+
+                    }
+                });
+        layout.addView(imageView, layoutParams);
+        bgLinearLayout.addView(layout);
+
+        _shareButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent share = new Intent(Intent.ACTION_SEND);
+                share.setType("image/jpeg");
+                share.putExtra(Intent.EXTRA_STREAM, file);
+                startActivity(Intent.createChooser(share, "Share Image"));
+            }
+        });
+
+
+    }
+
+    private void applyPalette(Palette palette, ViewGroup viewGroup) {
+        int bgColor = getBackgroundColor(palette);
+        if (bgColor != -1)
+            viewGroup.setBackgroundColor(bgColor);
+    }
+
+    private void applyPalette(Palette palette, View view) {
+        int bgColor = getBackgroundColor(palette);
+        if (bgColor != -1)
+            view.setBackgroundColor(bgColor);
+    }
+
+    private int getBackgroundColor(Palette palette) {
+        int bgColor = -1;
+
+        int vibrantColor = palette.getVibrantColor(0x000000);
+        int lightVibrantColor = palette.getLightVibrantColor(0x000000);
+        int darkVibrantColor = palette.getDarkVibrantColor(0x000000);
+
+        int mutedColor = palette.getMutedColor(0x000000);
+        int lightMutedColor = palette.getLightMutedColor(0x000000);
+        int darkMutedColor = palette.getDarkMutedColor(0x000000);
+
+        if (paletteColorType != null) {
+            switch (paletteColorType) {
+                case VIBRANT:
+                    if (vibrantColor != 0) { // primary option
+                        bgColor = vibrantColor;
+                    } else if (lightVibrantColor != 0) { // fallback options
+                        bgColor = lightVibrantColor;
+                    } else if (darkVibrantColor != 0) {
+                        bgColor = darkVibrantColor;
+                    } else if (mutedColor != 0) {
+                        bgColor = mutedColor;
+                    } else if (lightMutedColor != 0) {
+                        bgColor = lightMutedColor;
+                    } else if (darkMutedColor != 0) {
+                        bgColor = darkMutedColor;
+                    }
+                    break;
+                case LIGHT_VIBRANT:
+                    if (lightVibrantColor != 0) { // primary option
+                        bgColor = lightVibrantColor;
+                    } else if (vibrantColor != 0) { // fallback options
+                        bgColor = vibrantColor;
+                    } else if (darkVibrantColor != 0) {
+                        bgColor = darkVibrantColor;
+                    } else if (mutedColor != 0) {
+                        bgColor = mutedColor;
+                    } else if (lightMutedColor != 0) {
+                        bgColor = lightMutedColor;
+                    } else if (darkMutedColor != 0) {
+                        bgColor = darkMutedColor;
+                    }
+                    break;
+                case DARK_VIBRANT:
+                    if (darkVibrantColor != 0) { // primary option
+                        bgColor = darkVibrantColor;
+                    } else if (vibrantColor != 0) { // fallback options
+                        bgColor = vibrantColor;
+                    } else if (lightVibrantColor != 0) {
+                        bgColor = lightVibrantColor;
+                    } else if (mutedColor != 0) {
+                        bgColor = mutedColor;
+                    } else if (lightMutedColor != 0) {
+                        bgColor = lightMutedColor;
+                    } else if (darkMutedColor != 0) {
+                        bgColor = darkMutedColor;
+                    }
+                    break;
+                case MUTED:
+                    if (mutedColor != 0) { // primary option
+                        bgColor = mutedColor;
+                    } else if (lightMutedColor != 0) { // fallback options
+                        bgColor = lightMutedColor;
+                    } else if (darkMutedColor != 0) {
+                        bgColor = darkMutedColor;
+                    } else if (vibrantColor != 0) {
+                        bgColor = vibrantColor;
+                    } else if (lightVibrantColor != 0) {
+                        bgColor = lightVibrantColor;
+                    } else if (darkVibrantColor != 0) {
+                        bgColor = darkVibrantColor;
+                    }
+                    break;
+                case LIGHT_MUTED:
+                    if (lightMutedColor != 0) { // primary option
+                        bgColor = lightMutedColor;
+                    } else if (mutedColor != 0) { // fallback options
+                        bgColor = mutedColor;
+                    } else if (darkMutedColor != 0) {
+                        bgColor = darkMutedColor;
+                    } else if (vibrantColor != 0) {
+                        bgColor = vibrantColor;
+                    } else if (lightVibrantColor != 0) {
+                        bgColor = lightVibrantColor;
+                    } else if (darkVibrantColor != 0) {
+                        bgColor = darkVibrantColor;
+                    }
+                    break;
+                case DARK_MUTED:
+                    if (darkMutedColor != 0) { // primary option
+                        bgColor = darkMutedColor;
+                    } else if (mutedColor != 0) { // fallback options
+                        bgColor = mutedColor;
+                    } else if (lightMutedColor != 0) {
+                        bgColor = lightMutedColor;
+                    } else if (vibrantColor != 0) {
+                        bgColor = vibrantColor;
+                    } else if (lightVibrantColor != 0) {
+                        bgColor = lightVibrantColor;
+                    } else if (darkVibrantColor != 0) {
+                        bgColor = darkVibrantColor;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        return bgColor;
+    }
+}
--- app/src/main/java/com/example/narco/one_click/Drawer/Postcardsfragment.java
+++ app/src/main/java/com/example/narco/one_click/Drawer/Postcardsfragment.java
@@ -1,40 +1,26 @@
 package com.example.narco.one_click.Drawer;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
-import android.support.v7.graphics.Palette;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
-import com.etiennelawlor.imagegallery.library.ImageGalleryFragment;
-import com.etiennelawlor.imagegallery.library.activities.FullScreenImageGalleryActivity;
-import com.etiennelawlor.imagegallery.library.activities.ImageGalleryActivity;
-import com.etiennelawlor.imagegallery.library.adapters.FullScreenImageGalleryAdapter;
-import com.etiennelawlor.imagegallery.library.adapters.ImageGalleryAdapter;
-import com.etiennelawlor.imagegallery.library.enums.PaletteColorType;
 import com.example.narco.one_click.R;
-import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import butterknife.ButterKnife;
-import butterknife.OnClick;
+import java.io.File;
 
 
-public class Postcardsfragment extends Fragment implements ImageGalleryAdapter.ImageThumbnailLoader, FullScreenImageGalleryAdapter.FullScreenImageLoader {
+public class Postcardsfragment extends Fragment {
 
-    // region Member Variables
-    private PaletteColorType paletteColorType;
-    // endregion
+    private File[] listFile;
+    File file;
 
     public static Postcardsfragment newInstance() {
         return new Postcardsfragment();
@@ -48,198 +34,64 @@ public class Postcardsfragment extends Fragment implements ImageGalleryAdapter.I
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.test2, parent, false);
+        View v = inflater.inflate(R.layout.activity_postcatds, parent, false);
         getActivity().setTitle(R.string.postcards_menu_title);
-        ButterKnife.bind(getActivity());
+        LinearLayout linearLayout = (LinearLayout) v.findViewById(R.id.main_layout);
 
-        ImageGalleryActivity.setImageThumbnailLoader(this);
-        ImageGalleryFragment.setImageThumbnailLoader(this);
-        FullScreenImageGalleryActivity.setFullScreenImageLoader(this);
-
-        paletteColorType = PaletteColorType.VIBRANT;
-        return v;
-    }
-
-    // region Listeners
-    @OnClick(R.id.image_gallery_activity_btn)
-    public void onImageGalleryActivityButtonClicked() {
-        Intent intent = new Intent(getActivity(), ImageGalleryActivity.class);
-
-        String[] images = getResources().getStringArray(R.array.unsplash_images);
-        Bundle bundle = new Bundle();
-        bundle.putStringArrayList(ImageGalleryActivity.KEY_IMAGES, new ArrayList<>(Arrays.asList(images)));
-        bundle.putString(ImageGalleryActivity.KEY_TITLE, "Unsplash Images");
-        intent.putExtras(bundle);
-
-        startActivity(intent);
-    }
-
-    // region ImageGalleryAdapter.ImageThumbnailLoader Methods
-    @Override
-    public void loadImageThumbnail(final ImageView iv, String imageUrl, int dimension) {
-        if (!TextUtils.isEmpty(imageUrl)) {
-            Picasso.with(iv.getContext())
-                    .load(imageUrl)
-                    .resize(dimension, dimension)
-                    .centerCrop()
-                    .into(iv);
+        if (!Environment.getExternalStorageState().equals(
+                Environment.MEDIA_MOUNTED)) {
+            Toast.makeText(getActivity(), "Error! No SDCARD Found!", Toast.LENGTH_LONG)
+                    .show();
         } else {
-            iv.setImageDrawable(null);
+            // Locate the image folder in your SD Card
+            file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+                    + File.separator + "One_Click_Pics");
         }
-    }
-    // endregion
 
-    // region FullScreenImageGalleryAdapter.FullScreenImageLoader
-    @Override
-    public void loadFullScreenImage(final ImageView iv, String imageUrl, int width, final LinearLayout bgLinearLayout) {
-        if (!TextUtils.isEmpty(imageUrl)) {
-            Picasso.with(iv.getContext())
-                    .load(imageUrl)
-                    .resize(width, 0)
-                    .into(iv, new Callback() {
-                        @Override
-                        public void onSuccess() {
-                            Bitmap bitmap = ((BitmapDrawable) iv.getDrawable()).getBitmap();
-                            Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
-                                public void onGenerated(Palette palette) {
-                                    applyPalette(palette, bgLinearLayout);
-                                }
-                            });
-                        }
+        if (file.isDirectory()) {
+            listFile = file.listFiles();
+            // Create a String array for FilePathStrings
+            String[] filePathStrings;
+            filePathStrings = new String[listFile.length];
+            // Create a String array for FileNameStrings
+            String[] fileNameStrings;
+            fileNameStrings = new String[listFile.length];
 
-                        @Override
-                        public void onError() {
-
-                        }
-                    });
-        } else {
-            iv.setImageDrawable(null);
-        }
-    }
-    // endregion
-
-    // region Helper Methods
-    private void applyPalette(Palette palette, ViewGroup viewGroup){
-        int bgColor = getBackgroundColor(palette);
-        if (bgColor != -1)
-            viewGroup.setBackgroundColor(bgColor);
-    }
-
-    private void applyPalette(Palette palette, View view){
-        int bgColor = getBackgroundColor(palette);
-        if (bgColor != -1)
-            view.setBackgroundColor(bgColor);
-    }
-
-    private int getBackgroundColor(Palette palette) {
-        int bgColor = -1;
-
-        int vibrantColor = palette.getVibrantColor(0x000000);
-        int lightVibrantColor = palette.getLightVibrantColor(0x000000);
-        int darkVibrantColor = palette.getDarkVibrantColor(0x000000);
-
-        int mutedColor = palette.getMutedColor(0x000000);
-        int lightMutedColor = palette.getLightMutedColor(0x000000);
-        int darkMutedColor = palette.getDarkMutedColor(0x000000);
-
-        if (paletteColorType != null) {
-            switch (paletteColorType) {
-                case VIBRANT:
-                    if (vibrantColor != 0) { // primary option
-                        bgColor = vibrantColor;
-                    } else if (lightVibrantColor != 0) { // fallback options
-                        bgColor = lightVibrantColor;
-                    } else if (darkVibrantColor != 0) {
-                        bgColor = darkVibrantColor;
-                    } else if (mutedColor != 0) {
-                        bgColor = mutedColor;
-                    } else if (lightMutedColor != 0) {
-                        bgColor = lightMutedColor;
-                    } else if (darkMutedColor != 0) {
-                        bgColor = darkMutedColor;
-                    }
-                    break;
-                case LIGHT_VIBRANT:
-                    if (lightVibrantColor != 0) { // primary option
-                        bgColor = lightVibrantColor;
-                    } else if (vibrantColor != 0) { // fallback options
-                        bgColor = vibrantColor;
-                    } else if (darkVibrantColor != 0) {
-                        bgColor = darkVibrantColor;
-                    } else if (mutedColor != 0) {
-                        bgColor = mutedColor;
-                    } else if (lightMutedColor != 0) {
-                        bgColor = lightMutedColor;
-                    } else if (darkMutedColor != 0) {
-                        bgColor = darkMutedColor;
-                    }
-                    break;
-                case DARK_VIBRANT:
-                    if (darkVibrantColor != 0) { // primary option
-                        bgColor = darkVibrantColor;
-                    } else if (vibrantColor != 0) { // fallback options
-                        bgColor = vibrantColor;
-                    } else if (lightVibrantColor != 0) {
-                        bgColor = lightVibrantColor;
-                    } else if (mutedColor != 0) {
-                        bgColor = mutedColor;
-                    } else if (lightMutedColor != 0) {
-                        bgColor = lightMutedColor;
-                    } else if (darkMutedColor != 0) {
-                        bgColor = darkMutedColor;
-                    }
-                    break;
-                case MUTED:
-                    if (mutedColor != 0) { // primary option
-                        bgColor = mutedColor;
-                    } else if (lightMutedColor != 0) { // fallback options
-                        bgColor = lightMutedColor;
-                    } else if (darkMutedColor != 0) {
-                        bgColor = darkMutedColor;
-                    } else if (vibrantColor != 0) {
-                        bgColor = vibrantColor;
-                    } else if (lightVibrantColor != 0) {
-                        bgColor = lightVibrantColor;
-                    } else if (darkVibrantColor != 0) {
-                        bgColor = darkVibrantColor;
-                    }
-                    break;
-                case LIGHT_MUTED:
-                    if (lightMutedColor != 0) { // primary option
-                        bgColor = lightMutedColor;
-                    } else if (mutedColor != 0) { // fallback options
-                        bgColor = mutedColor;
-                    } else if (darkMutedColor != 0) {
-                        bgColor = darkMutedColor;
-                    } else if (vibrantColor != 0) {
-                        bgColor = vibrantColor;
-                    } else if (lightVibrantColor != 0) {
-                        bgColor = lightVibrantColor;
-                    } else if (darkVibrantColor != 0) {
-                        bgColor = darkVibrantColor;
-                    }
-                    break;
-                case DARK_MUTED:
-                    if (darkMutedColor != 0) { // primary option
-                        bgColor = darkMutedColor;
-                    } else if (mutedColor != 0) { // fallback options
-                        bgColor = mutedColor;
-                    } else if (lightMutedColor != 0) {
-                        bgColor = lightMutedColor;
-                    } else if (vibrantColor != 0) {
-                        bgColor = vibrantColor;
-                    } else if (lightVibrantColor != 0) {
-                        bgColor = lightVibrantColor;
-                    } else if (darkVibrantColor != 0) {
-                        bgColor = darkVibrantColor;
-                    }
-                    break;
-                default:
-                    break;
+            for (int i = 0; i < listFile.length; i++) {
+                // Get the path of the image file
+                filePathStrings[i] = listFile[i].getAbsolutePath();
+                // Get the name image file
+                fileNameStrings[i] = listFile[i].getName();
             }
         }
 
-        return bgColor;
+        for (final File file : listFile) {
+            ImageView imageView = new ImageView(getActivity());
+            LinearLayout layout = new LinearLayout(getActivity());
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            layoutParams.setMargins(0, 100, 0, 100);
+            Picasso.with(getActivity())
+                    .load(file)
+                    .resize(400, 750)
+                    .centerCrop()
+                    .into(imageView);
+
+            imageView.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+
+                    Intent i = new Intent(getActivity(), FullScreenImageActivity.class);
+                    i.putExtra("FILE", file);
+                    startActivity(i);
+
+                }
+            });
+
+            layout.addView(imageView, layoutParams);
+            linearLayout.addView(layout);
+        }
+        return v;
     }
-    // endregion
+
+
 }
